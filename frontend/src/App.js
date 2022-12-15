c27ad463ab86be71d31086ca74cb247edc941439
Wyero JR <kajuwyero@gmail.com>
2022-12-15 18:39:23 +0700

    master

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header text-center font-sans text-lg text-blue-500">
+        <h2>Yayasan Cendikia</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
