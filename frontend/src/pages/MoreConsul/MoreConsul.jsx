894f4b8707b8e793900c7ba4537d0e6e8c8f03b1
Wyero JR <kajuwyero@gmail.com>
2022-12-29 16:06:25 +0700

    test

--- frontend/src/pages/MoreConsul/MoreConsul.jsx
+++ frontend/src/pages/MoreConsul/MoreConsul.jsx
@@ -20,209 +20,116 @@ const MoreConsul = () => {
           </div>
           <div className='flex flex-wrap justify-center gap-6'>
             <div className=''>
-              <div className='flex border-[1px] border-gray1 rounded-md w-[384px] h-[160px] p-[16px]'>
-                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full'/>
-                <div className='font-inter w-[264px] ml-2'>
-                  <p className='text-lg font-600 text-dark'>dr. Lorem Ipsum</p>
-                  <p className='text-gray2'>Keahlian Lorem Ipsum</p>
-                  <div className='text-gray2 text-[14px] font-600'>
-                    <div className='flex items-center'>
-                      <MdWork className='text-[17px] mr-2'/>
-                      <p>6 Tahun</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <AiFillLike className='text-[17px] mr-2'/>
-                      <p>4.9/5</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <FaCalendarAlt className='text-[17px] mr-2'/>
-                      <p>Senin-Rabu 07:00-11:00</p>
-                    </div>
-                  </div>
+              <div className='flex flex-col items-center border-[1px] border-gray1 rounded-md w-[284px] h-[248px] p-[16px]'>
+                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full mb-2'/>
+                <p className='font-inter font-600 text-[18px] text-dark mb-1'>dr. Lorem Ipsum</p>
+                <p className='font-inter text-gray2'>Keahlian Lorem Ipsum</p>
+                <div className='font-inter text-[14px] flex items-center text-gray2 mb-1'>
+                  <MdWork className='mr-[9.67px] text-[17px]'/>
+                  <p>6 Tahun</p>
+                </div>
+                <div className='font-inter text-[14px] flex items-center text-gray2 mb-1'>
+                  <AiFillLike className='mr-[9.67px] text-[17px]'/>
+                  <p>4.9/5</p>
+                </div>
+                <div className='font-inter text-[14px] flex items-center text-gray2'>
+                  <FaCalendarAlt className='mr-[9.67px] text-[17px]'/>
+                  <p>Senin-Rabu 07:00-11:00</p>
                 </div>
               </div>
             </div>
             <div className=''>
-              <div className='flex border-[1px] border-gray1 rounded-md w-[384px] h-[160px] p-[16px]'>
-                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full'/>
-                <div className='font-inter w-[264px] ml-2'>
-                  <p className='text-lg font-600 text-dark'>dr. Lorem Ipsum</p>
-                  <p className='text-gray2'>Keahlian Lorem Ipsum</p>
-                  <div className='text-gray2 text-[14px] font-600'>
-                    <div className='flex items-center'>
-                      <MdWork className='text-[17px] mr-2'/>
-                      <p>6 Tahun</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <AiFillLike className='text-[17px] mr-2'/>
-                      <p>4.9/5</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <FaCalendarAlt className='text-[17px] mr-2'/>
-                      <p>Senin-Rabu 07:00-11:00</p>
-                    </div>
-                  </div>
+              <div className='flex flex-col items-center border-[1px] border-gray1 rounded-md w-[284px] h-[248px] p-[16px]'>
+                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full mb-2'/>
+                <p className='font-inter font-600 text-[18px] text-dark mb-1'>dr. Lorem Ipsum</p>
+                <p className='font-inter text-gray2'>Keahlian Lorem Ipsum</p>
+                <div className='font-inter text-[14px] flex items-center text-gray2 mb-1'>
+                  <MdWork className='mr-[9.67px] text-[17px]'/>
+                  <p>6 Tahun</p>
+                </div>
+                <div className='font-inter text-[14px] flex items-center text-gray2 mb-1'>
+                  <AiFillLike className='mr-[9.67px] text-[17px]'/>
+                  <p>4.9/5</p>
+                </div>
+                <div className='font-inter text-[14px] flex items-center text-gray2'>
+                  <FaCalendarAlt className='mr-[9.67px] text-[17px]'/>
+                  <p>Senin-Rabu 07:00-11:00</p>
                 </div>
               </div>
             </div>
             <div className=''>
-              <div className='flex border-[1px] border-gray1 rounded-md w-[384px] h-[160px] p-[16px]'>
-                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full'/>
-                <div className='font-inter w-[264px] ml-2'>
-                  <p className='text-lg font-600 text-dark'>dr. Lorem Ipsum</p>
-                  <p className='text-gray2'>Keahlian Lorem Ipsum</p>
-                  <div className='text-gray2 text-[14px] font-600'>
-                    <div className='flex items-center'>
-                      <MdWork className='text-[17px] mr-2'/>
-                      <p>6 Tahun</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <AiFillLike className='text-[17px] mr-2'/>
-                      <p>4.9/5</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <FaCalendarAlt className='text-[17px] mr-2'/>
-                      <p>Senin-Rabu 07:00-11:00</p>
-                    </div>
-                  </div>
+              <div className='flex flex-col items-center border-[1px] border-gray1 rounded-md w-[284px] h-[248px] p-[16px]'>
+                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full mb-2'/>
+                <p className='font-inter font-600 text-[18px] text-dark mb-1'>dr. Lorem Ipsum</p>
+                <p className='font-inter text-gray2'>Keahlian Lorem Ipsum</p>
+                <div className='font-inter text-[14px] flex items-center text-gray2 mb-1'>
+                  <MdWork className='mr-[9.67px] text-[17px]'/>
+                  <p>6 Tahun</p>
+                </div>
+                <div className='font-inter text-[14px] flex items-center text-gray2 mb-1'>
+                  <AiFillLike className='mr-[9.67px] text-[17px]'/>
+                  <p>4.9/5</p>
+                </div>
+                <div className='font-inter text-[14px] flex items-center text-gray2'>
+                  <FaCalendarAlt className='mr-[9.67px] text-[17px]'/>
+                  <p>Senin-Rabu 07:00-11:00</p>
                 </div>
               </div>
             </div>
             <div className=''>
-              <div className='flex border-[1px] border-gray1 rounded-md w-[384px] h-[160px] p-[16px]'>
-                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full'/>
-                <div className='font-inter w-[264px] ml-2'>
-                  <p className='text-lg font-600 text-dark'>dr. Lorem Ipsum</p>
-                  <p className='text-gray2'>Keahlian Lorem Ipsum</p>
-                  <div className='text-gray2 text-[14px] font-600'>
-                    <div className='flex items-center'>
-                      <MdWork className='text-[17px] mr-2'/>
-                      <p>6 Tahun</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <AiFillLike className='text-[17px] mr-2'/>
-                      <p>4.9/5</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <FaCalendarAlt className='text-[17px] mr-2'/>
-                      <p>Senin-Rabu 07:00-11:00</p>
-                    </div>
-                  </div>
+              <div className='flex flex-col items-center border-[1px] border-gray1 rounded-md w-[284px] h-[248px] p-[16px]'>
+                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full mb-2'/>
+                <p className='font-inter font-600 text-[18px] text-dark mb-1'>dr. Lorem Ipsum</p>
+                <p className='font-inter text-gray2'>Keahlian Lorem Ipsum</p>
+                <div className='font-inter text-[14px] flex items-center text-gray2 mb-1'>
+                  <MdWork className='mr-[9.67px] text-[17px]'/>
+                  <p>6 Tahun</p>
+                </div>
+                <div className='font-inter text-[14px] flex items-center text-gray2 mb-1'>
+                  <AiFillLike className='mr-[9.67px] text-[17px]'/>
+                  <p>4.9/5</p>
+                </div>
+                <div className='font-inter text-[14px] flex items-center text-gray2'>
+                  <FaCalendarAlt className='mr-[9.67px] text-[17px]'/>
+                  <p>Senin-Rabu 07:00-11:00</p>
                 </div>
               </div>
             </div>
             <div className=''>
-              <div className='flex border-[1px] border-gray1 rounded-md w-[384px] h-[160px] p-[16px]'>
-                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full'/>
-                <div className='font-inter w-[264px] ml-2'>
-                  <p className='text-lg font-600 text-dark'>dr. Lorem Ipsum</p>
-                  <p className='text-gray2'>Keahlian Lorem Ipsum</p>
-                  <div className='text-gray2 text-[14px] font-600'>
-                    <div className='flex items-center'>
-                      <MdWork className='text-[17px] mr-2'/>
-                      <p>6 Tahun</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <AiFillLike className='text-[17px] mr-2'/>
-                      <p>4.9/5</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <FaCalendarAlt className='text-[17px] mr-2'/>
-                      <p>Senin-Rabu 07:00-11:00</p>
-                    </div>
-                  </div>
+              <div className='flex flex-col items-center border-[1px] border-gray1 rounded-md w-[284px] h-[248px] p-[16px]'>
+                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full mb-2'/>
+                <p className='font-inter font-600 text-[18px] text-dark mb-1'>dr. Lorem Ipsum</p>
+                <p className='font-inter text-gray2'>Keahlian Lorem Ipsum</p>
+                <div className='font-inter text-[14px] flex items-center text-gray2 mb-1'>
+                  <MdWork className='mr-[9.67px] text-[17px]'/>
+                  <p>6 Tahun</p>
+                </div>
+                <div className='font-inter text-[14px] flex items-center text-gray2 mb-1'>
+                  <AiFillLike className='mr-[9.67px] text-[17px]'/>
+                  <p>4.9/5</p>
+                </div>
+                <div className='font-inter text-[14px] flex items-center text-gray2'>
+                  <FaCalendarAlt className='mr-[9.67px] text-[17px]'/>
+                  <p>Senin-Rabu 07:00-11:00</p>
                 </div>
               </div>
             </div>
             <div className=''>
-              <div className='flex border-[1px] border-gray1 rounded-md w-[384px] h-[160px] p-[16px]'>
-                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full'/>
-                <div className='font-inter w-[264px] ml-2'>
-                  <p className='text-lg font-600 text-dark'>dr. Lorem Ipsum</p>
-                  <p className='text-gray2'>Keahlian Lorem Ipsum</p>
-                  <div className='text-gray2 text-[14px] font-600'>
-                    <div className='flex items-center'>
-                      <MdWork className='text-[17px] mr-2'/>
-                      <p>6 Tahun</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <AiFillLike className='text-[17px] mr-2'/>
-                      <p>4.9/5</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <FaCalendarAlt className='text-[17px] mr-2'/>
-                      <p>Senin-Rabu 07:00-11:00</p>
-                    </div>
-                  </div>
+              <div className='flex flex-col items-center border-[1px] border-gray1 rounded-md w-[284px] h-[248px] p-[16px]'>
+                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full mb-2'/>
+                <p className='font-inter font-600 text-[18px] text-dark mb-1'>dr. Lorem Ipsum</p>
+                <p className='font-inter text-gray2'>Keahlian Lorem Ipsum</p>
+                <div className='font-inter text-[14px] flex items-center text-gray2 mb-1'>
+                  <MdWork className='mr-[9.67px] text-[17px]'/>
+                  <p>6 Tahun</p>
                 </div>
-              </div>
-            </div>
-            <div className=''>
-              <div className='flex border-[1px] border-gray1 rounded-md w-[384px] h-[160px] p-[16px]'>
-                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full'/>
-                <div className='font-inter w-[264px] ml-2'>
-                  <p className='text-lg font-600 text-dark'>dr. Lorem Ipsum</p>
-                  <p className='text-gray2'>Keahlian Lorem Ipsum</p>
-                  <div className='text-gray2 text-[14px] font-600'>
-                    <div className='flex items-center'>
-                      <MdWork className='text-[17px] mr-2'/>
-                      <p>6 Tahun</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <AiFillLike className='text-[17px] mr-2'/>
-                      <p>4.9/5</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <FaCalendarAlt className='text-[17px] mr-2'/>
-                      <p>Senin-Rabu 07:00-11:00</p>
-                    </div>
-                  </div>
+                <div className='font-inter text-[14px] flex items-center text-gray2 mb-1'>
+                  <AiFillLike className='mr-[9.67px] text-[17px]'/>
+                  <p>4.9/5</p>
                 </div>
-              </div>
-            </div>
-            <div className=''>
-              <div className='flex border-[1px] border-gray1 rounded-md w-[384px] h-[160px] p-[16px]'>
-                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full'/>
-                <div className='font-inter w-[264px] ml-2'>
-                  <p className='text-lg font-600 text-[#262626]'>dr. Lorem Ipsum</p>
-                  <p className='text-gray2'>Keahlian Lorem Ipsum</p>
-                  <div className='text-gray2 text-[14px] font-600'>
-                    <div className='flex items-center'>
-                      <MdWork className='text-[17px] mr-2'/>
-                      <p>6 Tahun</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <AiFillLike className='text-[17px] mr-2'/>
-                      <p>4.9/5</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <FaCalendarAlt className='text-[17px] mr-2'/>
-                      <p>Senin-Rabu 07:00-11:00</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className=''>
-              <div className='flex border-[1px] border-gray1 rounded-md w-[384px] h-[160px] p-[16px]'>
-                <img src={profile} alt={profile} className='w-[80px] h-[80px] rounded-full'/>
-                <div className='font-inter w-[264px] ml-2'>
-                  <p className='text-lg font-600 text-[#262626]'>dr. Lorem Ipsum</p>
-                  <p className='text-gray2'>Keahlian Lorem Ipsum</p>
-                  <div className='text-gray2 text-[14px] font-600'>
-                    <div className='flex items-center'>
-                      <MdWork className='text-[17px] mr-2'/>
-                      <p>6 Tahun</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <AiFillLike className='text-[17px] mr-2'/>
-                      <p>4.9/5</p>
-                    </div>
-                    <div className='flex items-center'>
-                      <FaCalendarAlt className='text-[17px] mr-2'/>
-                      <p>Senin-Rabu 07:00-11:00</p>
-                    </div>
-                  </div>
+                <div className='font-inter text-[14px] flex items-center text-gray2'>
+                  <FaCalendarAlt className='mr-[9.67px] text-[17px]'/>
+                  <p>Senin-Rabu 07:00-11:00</p>
                 </div>
               </div>
             </div>
