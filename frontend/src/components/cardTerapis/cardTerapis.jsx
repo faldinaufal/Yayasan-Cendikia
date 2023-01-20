1ad7467119928a3e2419cce0f79bd9446100cce0
faldinaufal <faldinaufa11@gmail.com>
2023-01-20 19:11:44 +0700

    submit

--- frontend/src/components/cardTerapis/cardTerapis.jsx
+++ frontend/src/components/cardTerapis/cardTerapis.jsx
@@ -5,20 +5,20 @@ import {FaCalendarAlt} from 'react-icons/fa'
 const TherapistCard = ({index}) => {
     return (
         <div>
-            <a href={`/${index.terapisId.UID}/consultation`}>
+            <a href={`/${index.therapist.UID}/consultation`}>
                 <div className='flex border-[1px] border-gray1 rounded-md w-[384px] h-[160px] p-[16px]'>
                     <img src={process.env.REACT_APP_API_URL+index.photoProfile.url} alt="Foto Profil" className='w-[80px] h-[80px] rounded-full'/>
                 <div className='font-inter w-[264px] ml-2'>
                     <p className='text-lg font-600 text-dark'>{index.username}</p>
-                    <p className='text-gray2'>{index.terapisId.Skill}</p>
+                    <p className='text-gray2'>{index.therapist.skill}</p>
                     <div className='text-gray2 text-[14px] font-600'>
                     <div className='flex items-center'>
                         <MdWork className='text-[17px] mr-2'/>
-                        <p>{index.terapisId.Experiences}</p>
+                        <p>{index.therapist.experience}</p>
                     </div>
                     <div className='flex items-center'>
                         <FaCalendarAlt className='text-[17px] mr-2'/>
-                        <p>Senin-Rabu 07:00-11:00</p>
+                        <p>{index.therapist.jobTime}</p>
                     </div>
                     </div>
                 </div>
